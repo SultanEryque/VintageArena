0817c8b233af80fc01ed647a031ebd70b194787e
SultanEryque <eryquemills33@gmail.com>
2020-05-17 19:48:11 +0300

    importing datetime feature for python web based apps

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-	headline = "PYTHON AGGRESSIVE"
-	return render_template ("python.html", headline=headline)
-
-@app.route("/cornelius")
-def cornelius():
-	headline = "PYTHON CORNELIUS"
-	return render_template ("python.html", headline=headline)
-
+def python():
+	now = datetime.datetime.now()
+	python_day = now.month == 5 and now.day == 17
+	return render_template ("python.html", python_day=python_day) 
